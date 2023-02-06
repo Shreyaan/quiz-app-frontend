d96deedd929eba69f887c24fd32f02c2d6f596ca
Shreyaan <Shreyaans41@gmail.com>
2023-02-07 02:43:45 +0530

    Revert "."
    This reverts commit d400e45ee9df1570a467d65b78fa3e0c24386a7f.

--- pages/quiz/[quiz].tsx
+++ pages/quiz/[quiz].tsx
@@ -113,9 +113,12 @@ const QuizPage = () => {
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100">
-          <li>
-                  <Link href="./">Home</Link>
-                </li>
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
           </ul>
         </div>
       </div>
--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -150,9 +150,12 @@ const Quiz: NextPage = () => {
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100">
-          <li>
-                  <Link href="">My account</Link>
-                </li>
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
           </ul>
         </div>
       </div>
