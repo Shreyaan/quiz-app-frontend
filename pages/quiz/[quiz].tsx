d400e45ee9df1570a467d65b78fa3e0c24386a7f
Shreyaan <Shreyaans41@gmail.com>
2023-02-07 02:33:59 +0530

    .

--- pages/quiz/[quiz].tsx
+++ pages/quiz/[quiz].tsx
@@ -113,12 +113,9 @@ const QuizPage = () => {
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100">
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
+          <li>
+                  <Link href="./">Home</Link>
+                </li>
           </ul>
         </div>
       </div>
--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -150,12 +150,9 @@ const Quiz: NextPage = () => {
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100">
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
+          <li>
+                  <Link href="">My account</Link>
+                </li>
           </ul>
         </div>
       </div>
