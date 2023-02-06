b882afcc64f2190f255dbe4d9bf806b39aa264be
Shreyaan <Shreyaans41@gmail.com>
2023-02-07 00:26:25 +0530

    .

--- pages/quiz/[quiz].tsx
+++ pages/quiz/[quiz].tsx
@@ -41,10 +41,16 @@ const QuizPage = () => {
   return (
     <div>
       {quiz && (
-        <div>
-          <h2>{quiz.Name}</h2> <p>Created by {quiz.created_by}</p>
-          {quiz.image && <img src={quiz.image} alt={quiz.Name} />}
-        </div>
+       <div className="hero min-h-screen bg-base-200">
+       <div className="hero-content flex-col lg:flex-row-reverse">
+         <img src={quiz.image}  className="max-w-sm rounded-lg shadow-2xl" />
+         <div>
+           <h1 className="text-5xl font-bold"> {quiz.Name} Quiz </h1>
+           <p className="py-6">Created by - {quiz.created_by}</p>
+           
+         </div>
+       </div>
+     </div>
       )}
     </div>
   );
