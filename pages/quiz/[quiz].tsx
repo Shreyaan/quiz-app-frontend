a7539087179ef4c65e53070666f92eae2f97198c
Shreyaan <Shreyaans41@gmail.com>
2023-02-07 02:33:17 +0530

    .

--- pages/quiz/[quiz].tsx
+++ pages/quiz/[quiz].tsx
@@ -5,6 +5,7 @@ import { useLocalStorage } from "usehooks-ts";
 import { QuestionsWithoutQuestions } from ".";
 import { api_url } from "../../utils/config";
 import { errorHandler } from "../../utils/errorHandler";
+import Link from "next/link";
 
 const QuizPage = () => {
   const router = useRouter();
@@ -40,13 +41,49 @@ const QuizPage = () => {
 
   return (
     <div>
-      {quiz && (
-        <div className="hero min-h-screen bg-base-200">
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <div className="w-full navbar bg-base-300">
+            <div className="flex-none lg:hidden">
+              <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-6 h-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+            <div className="flex-1 px-2 mx-2">Quiz App</div>
+            <div className="flex-none hidden lg:block">
+              <ul className="menu menu-horizontal">
+                <li>
+                  <Link href="./">Home</Link>
+                </li>
+                {/* <li>
+                  <a>Navbar Item 2</a>
+                </li> */}
+              </ul>
+            </div>
+          </div>
+          {quiz && (
+        <div className="hero  bg-base-200">
           <div className="hero-content flex-col">
             <div>
               <h1 className="text-5xl font-bold"> {quiz.Name} Quiz </h1>
               <p className="py-6">Created by - {quiz.created_by}</p>
-            <img src={quiz.image} className=" md:max-w-sm rounded-lg shadow-2xl mx-auto my-5 mb-10 max-h-64 max-w-[90vw]" />
+              <img
+                src={quiz.image}
+                className=" md:max-w-sm rounded-lg shadow-2xl mx-auto my-5 mb-10 max-h-64 max-w-[90vw]"
+              />
               {quiz.questions &&
                 quiz.questions.map((question, index) => {
                   return (
@@ -60,10 +97,10 @@ const QuizPage = () => {
                     })}
                     </ul> */}
                       <div className="my-4">
-                          <p>a : {question.options.a}</p>
-                          <p> b : {question.options.b} </p>
-                          <p> c : {question.options.c}</p>
-                          <p> d : {question.options.d}</p>
+                        <p>a : {question.options.a}</p>
+                        <p> b : {question.options.b} </p>
+                        <p> c : {question.options.c}</p>
+                        <p> d : {question.options.d}</p>
                       </div>
                     </div>
                   );
@@ -72,6 +109,20 @@ const QuizPage = () => {
           </div>
         </div>
       )}
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-base-100">
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    
     </div>
   );
 };
